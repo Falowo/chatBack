36789a7071fecdbac3011cd7dfd641369d6151c8
josselin <obarayego@gmail.com>
2022-12-21 22:38:38 +0100

    bla

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,12 +2,14 @@ const mongoose = require("mongoose");
 
 const uri = process.env.MONGODB_URI;
 
-mongoose.connect(uri, () => {
-  console.log("connexion ok !");
-});
+mongoose.set("strictQuery", false);
 
 mongoose.connection.on("error", (e: Error) =>
   console.log(e),
 );
+
+mongoose.connect(uri, () => {
+  console.log("connexion ok !");
+});
 
 module.exports = mongoose;
