5a95a214e866972400b978c31e21995430b51828
josselin <obarayego@gmail.com>
2022-12-21 21:44:24 +0100

    mongoose connection slightly changed

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,13 +2,12 @@ const mongoose = require("mongoose");
 
 const uri = process.env.MONGODB_URI;
 
-mongoose
-  .connect(uri)
-  .then(() => {
-    console.log("connexion ok !");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+mongoose.connect(uri, () => {
+  console.log("connexion ok !");
+});
+
+mongoose.connection.on("error", (e: Error) =>
+  console.log(e),
+);
 
 module.exports = mongoose;
